af46c078139a7eaf70c4e071469e540c6bb1985f
manjunathlc <www.manjuchitagubbi96@gmail.com>
2024-07-30 19:54:10 +0530

    Initial Commit

---
+++ src/main/java/edu/org/ShopperStack1/ShopperStack1Application.java
@@ -0,0 +1,13 @@
+package edu.org.ShopperStack1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperStack1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopperStack1Application.class, args);
+	}
+
+}
